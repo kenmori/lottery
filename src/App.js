b840591bc2aff820312ae98079c6c93e1a9c05b0
kenmori <kenjimoritata@gmail.com>
2024-05-25 07:47:21 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Web3 from "./web3";
 import lottery from "./lottery";
 
 const App = () => {
-  const [state, setState] = React.useState({manager: '', players: [], balance: ''});
+  const [state, setState] = React.useState({manager: '', players: [], balance: '', value: ''});
   useEffect(() => {
     async function fetchManager(){
       const manager = await lottery.methods.manager().call();
       const players = await lottery.methods.getPlayers().call();
       const balance = await Web3.eth.getBalance(lottery.options.address);
-      setState({manager, players, balance})
+      setState({manager, players, balance, value: ''})
     }
     fetchManager()
   }, []);
@@ -20,6 +20,11 @@ const App = () => {
        {state.manager}
        there ar currently {state.players.length} people entered,
        competing to win {Web3.utils.fromWei(state.balance, 'ether')} ether!
+
+       <div>
+        <input value={state.value} onChange={(event) => setState((draft) => ({...draft, value: event.target.value})) } />
+        <button>Enter</button>
+       </div>
       </div>
     );
   }
